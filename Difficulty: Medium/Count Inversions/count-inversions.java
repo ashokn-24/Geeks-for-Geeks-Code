7af4f7fc0e275df1c8d10214d21760495c502d26
Ashok N <91756916+ashokn-24@users.noreply.github.com>
2025-01-18 11:52:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for (int g = 0; g < t; g++) {
+            String[] str = (br.readLine()).trim().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            System.out.println(new Solution().inversionCount(arr));
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    
+    
+    static int merge(int[] arr,int low ,int mid, int high){
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //Modification 1: cnt variable to count the pairs:
+        int cnt = 0;
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                cnt += (mid - left + 1); //Modification 2
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt; // Modification 3
+    }
+    
+    static int mergeSort(int[] arr,int low ,int high){
+        int cnt = 0;
+        if(low >= high) return cnt;
+        int mid = (low + high) / 2;
+        
+        cnt += mergeSort(arr,low,mid);
+        cnt += mergeSort(arr,mid+1,high);
+        
+        cnt += merge(arr,low,mid,high);
+        
+        return cnt;
+    }
+    
+    static int inversionCount(int arr[]) {
+        
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+}
